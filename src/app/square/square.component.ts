e4dd420ca29fff6081e05bb14ad8aeed7394a044
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-10-18 15:45:30 -0700

    created the logic for tictactoe and created board and square components

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      <button>{{ value }}</button>
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | "O";
+
+}
