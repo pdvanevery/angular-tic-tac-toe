f6331884f3c0fff59c49a84f92aebb0ebf49bac3
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-10-19 15:11:51 -0700

    updated square component so styling comes from scss file and also added separate colors for each player

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-square',
   template: `
-      <button>{{ value }}</button>
+      <button mat-flat-button color="basic" *ngIf="!value">{{ value }}</button>
+      <button mat-flat-button color="accent" *ngIf="value == 'X'">{{ value }}</button>
+      <button mat-flat-button color="primary" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
 
   @Input() value: 'X' | 'O';
 
 }
+
+//button { width: 100%; height: 100%; font-size: 5em !important; background-color: lightblue;}
